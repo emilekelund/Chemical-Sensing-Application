a5da6f0f392cd52fda58c5f3b196a11d34d4ea1c
Emil Ekelund <emil.ekelund@outlook.com>
2020-04-24 12:11:41 +0200

    Setup of views and recyclerView in onCreate

--- app/src/main/java/com/example/resistancereader/ScanActivity.java
+++ app/src/main/java/com/example/resistancereader/ScanActivity.java
@@ -1,11 +1,15 @@
 package com.example.resistancereader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,5 +32,32 @@ public class ScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        mHandler = new Handler();
+        mDeviceList = new ArrayList<>();
+
+        //Setup of view and button
+        mScanInfoView = findViewById(R.id.scan_info);
+        Button startScanButton = findViewById(R.id.start_scan_button);
+        startScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDeviceList.clear();
+            }
+        });
+
+        // Setup recycler view and corresponding adapter
+        RecyclerView recyclerView = findViewById(R.id.scan_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mBtDeviceAdapter = new BtDeviceAdapter(mDeviceList,
+                new BtDeviceAdapter.IOnItemSelectedCallBack() {
+                    @Override
+                    public void onItemClicked(int position) {
+
+                    }
+                });
+
+        recyclerView.setAdapter(mBtDeviceAdapter);
     }
 }
