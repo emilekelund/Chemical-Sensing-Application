646db4be1e4a4e78bf89ace96cfadc4905aad3c0
Emil Ekelund <emil.ekelund@outlook.com>
2020-04-24 12:05:14 +0200

    Add data members

--- app/src/main/java/com/example/resistancereader/ScanActivity.java
+++ app/src/main/java/com/example/resistancereader/ScanActivity.java
@@ -2,9 +2,27 @@ package com.example.resistancereader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ScanActivity extends AppCompatActivity {
+
+    public static final String RESISTANCE = "Resistance";
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    private ArrayList<BluetoothDevice> mDeviceList;
+    private BtDeviceAdapter mBtDeviceAdapter;
+    private TextView mScanInfoView;
+
+    private static final long SCAN_PERIOD = 10000; // represented in milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
