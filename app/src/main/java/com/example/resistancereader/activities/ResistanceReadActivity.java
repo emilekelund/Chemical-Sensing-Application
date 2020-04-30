dc98e09f9be4cc1679c6b98236f4e8918010ca34
Emil Ekelund <emil.ekelund@outlook.com>
2020-04-30 09:53:45 +0200

    Update, resistance read almost done. Only need to act on the data.

--- app/src/main/java/com/example/resistancereader/activities/ResistanceReadActivity.java
+++ app/src/main/java/com/example/resistancereader/activities/ResistanceReadActivity.java
@@ -2,19 +2,31 @@ package com.example.resistancereader.activities;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
 import com.example.resistancereader.BleServices.BleResistanceService;
+import com.example.resistancereader.BleServices.GattActions;
 import com.example.resistancereader.R;
 import com.example.resistancereader.utilities.MsgUtils;
 
+import static com.example.resistancereader.BleServices.GattActions.ACTION_GATT_RESISTANCE_EVENTS;
+import static com.example.resistancereader.BleServices.GattActions.EVENT;
+
 public class ResistanceReadActivity extends Activity {
+    private static final String TAG = ResistanceReadActivity.class.getSimpleName();
 
     private BluetoothDevice mSelectedDevice = null;
     private TextView mResistanceView;
@@ -22,7 +34,7 @@ public class ResistanceReadActivity extends Activity {
     private TextView mStatusView;
     private String mDeviceAddress;
 
-    private BleResistanceService mBluetoothService;
+    private BleResistanceService mBluetoothLeService;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -45,6 +57,73 @@ public class ResistanceReadActivity extends Activity {
         } else {
             mDeviceView.setText(mSelectedDevice.getName());
         }
+
+        // Bind to BleImuService
+        // We use onResume or onStart to register a broadcastReceiver
+        Intent gattServiceIntent = new Intent(this, BleResistanceService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+    }
+
+    /*
+    Callback methods to manage the (BleImu)Service lifecycle.
+    */
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BleResistanceService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.i(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+            Log.i(TAG, "onServiceConnected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+            Log.i(TAG, "onServiceDisconnected");
+        }
+    };
+
+    /*
+    A BroadcastReceiver handling various events fired by the Service, see GattActions.Event.
+    */
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (ACTION_GATT_RESISTANCE_EVENTS.equals(action)) {
+                GattActions.Event event = (GattActions.Event) intent.getSerializableExtra(EVENT);
+                if (event != null) {
+                    switch (event) {
+                        case GATT_CONNECTED:
+                        case GATT_DISCONNECTED:
+                        case GATT_SERVICES_DISCOVERED:
+                        case RESISTANCE_SERVICE_DISCOVERED:
+                            mStatusView.setText(event.toString());
+                            mResistanceView.setText("-");
+                            break;
+                        case DATA_AVAILABLE:
+                            break;
+                        case RESISTANCE_SERVICE_NOT_AVAILABLE:
+                            mStatusView.setText(event.toString());
+                            break;
+                        default:
+                            mStatusView.setText(R.string.device_unreachable);
+                    }
+                }
+            }
+        }
+    };
+
+    // Intent filter for broadcast updates from BleHeartRateServices
+    private IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_GATT_RESISTANCE_EVENTS);
+        return intentFilter;
     }
 
 
