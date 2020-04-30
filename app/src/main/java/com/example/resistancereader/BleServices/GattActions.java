80322d10c15cb386e5e1c97197ac38b404b0bbae
Emil Ekelund <emil.ekelund@outlook.com>
2020-04-30 08:37:43 +0200

    Add the GattActions class

---
+++ app/src/main/java/com/example/resistancereader/BleServices/GattActions.java
@@ -0,0 +1,48 @@
+package com.example.resistancereader.BleServices;
+
+public class GattActions {
+
+    /**
+     * The action corresponding to IMU events from BleImuService.
+     * Intended for IntentFilters for a BroadcastReceiver.
+     */
+    public final static String ACTION_GATT_IMU_EVENTS =
+            "com.example.resistancereader.services.ACTION_GATT_IMU_EVENTS";
+
+    /**
+     * A flag for event info in intents (via intent.putExtra)
+     */
+    public final static String EVENT =
+            "com.example.resistancereader.services.EVENT";
+
+
+    /**
+     * A flag for IMU data in intent (via intent.putExtra)
+     */
+    public final static String IMU_DATA =
+            "com.example.resistancereader.services.IMU_DATA";
+
+
+    /**
+     * Events corresponding to Gatt status/events
+     */
+    public enum Event {
+        GATT_CONNECTED("Connected"),
+        GATT_DISCONNECTED("Disconnected"),
+        GATT_SERVICES_DISCOVERED("Services discovered"),
+        IMU_SERVICE_DISCOVERED("IMU Service"),
+        IMU_SERVICE_NOT_AVAILABLE("IMU service unavailable"),
+        DATA_AVAILABLE("Data available");
+
+        @Override
+        public String toString() {
+            return text;
+        }
+
+        private final String text;
+
+        Event(String text) {
+            this.text = text;
+        }
+    }
+}
