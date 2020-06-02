aa7d6af53d504fe2d9d888fdd56ca6a424923d5e
Emil Ekelund <emil.ekelund@outlook.com>
2020-06-02 16:31:02 +0200

    Remove temp view code

--- app/src/main/java/com/example/resistancereader/activities/ResistanceReadActivity.java
+++ app/src/main/java/com/example/resistancereader/activities/ResistanceReadActivity.java
@@ -33,7 +33,6 @@ public class ResistanceReadActivity extends Activity {
 
     private BluetoothDevice mSelectedDevice = null;
     private TextView mResistanceView;
-    private TextView mTempView;
     private TextView mDeviceView;
     private TextView mStatusView;
     private String mDeviceAddress;
@@ -49,7 +48,6 @@ public class ResistanceReadActivity extends Activity {
 
         // Setup UI references
         mResistanceView = findViewById(R.id.resistanceValueViewer);
-        mTempView = findViewById(R.id.temp_viewer);
         mDeviceView = findViewById(R.id.device_view);
         mStatusView = findViewById(R.id.status_view);
 
@@ -140,7 +138,6 @@ public class ResistanceReadActivity extends Activity {
                         case RESISTANCE_SERVICE_DISCOVERED:
                             mStatusView.setText(event.toString());
                             mResistanceView.setText(R.string.calculating_resistance);
-                            mTempView.setText(R.string.waiting_for_resistance);
                             break;
                         case DATA_AVAILABLE:
                             final double resistance = intent.getDoubleExtra(RESISTANCE_DATA,0);
@@ -153,7 +150,6 @@ public class ResistanceReadActivity extends Activity {
                                 }
                                 avgResistance = avgResistance / resistanceValues.size();
                                 mResistanceView.setText(String.format("%.3f M\u2126", (avgResistance * (1*Math.pow(10, -6)))));
-                                mTempView.setText("TEMP HERE");
                                 resistanceValues.clear();
                             }
 
