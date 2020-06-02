95af6b27fc50fc90eb258a28f795506c74d26e6b
Emil Ekelund <emil.ekelund@outlook.com>
2020-06-02 16:55:48 +0200

    Continue to setup chart

--- app/src/main/java/com/example/resistancereader/activities/ResistanceReadActivity.java
+++ app/src/main/java/com/example/resistancereader/activities/ResistanceReadActivity.java
@@ -23,7 +23,13 @@ import com.example.resistancereader.services.BleResistanceService;
 import com.example.resistancereader.services.GattActions;
 import com.example.resistancereader.utilities.MsgUtils;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 
@@ -93,6 +99,41 @@ public class ResistanceReadActivity extends Activity {
         LineData data = new LineData();
         data.setValueTextColor(Color.BLACK);
 
+        // add empty data
+        mChart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.BLACK);
+
+        // X-axis setup
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.BLACK);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        // Y-axis setup
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.BLACK);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setAxisMaximum(1.5f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        // Disable right Y-axis
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        mChart.getAxisLeft().setDrawGridLines(true);
+        mChart.getXAxis().setDrawGridLines(true);
+        mChart.setDrawBorders(false);
+
+        feedMultiple();
+
+
         // Bind to BleImuService
         // We use onResume or onStart to register a broadcastReceiver
         Intent gattServiceIntent = new Intent(this, BleResistanceService.class);
@@ -149,6 +190,77 @@ public class ResistanceReadActivity extends Activity {
             Log.i(TAG, "onServiceDisconnected");
         }
     };
+
+    /*
+    A method to add our resistance entries to the chart
+     */
+    private void addEntry(double resistanceValue) {
+        LineData data = mChart.getData();
+        ILineDataSet set = null;
+
+        if (data != null) {
+            set = data.getDataSetByIndex(0);
+            // set.addEntry(.....); Can be called as well
+        }
+
+        if (set == null) {
+            set = createSet();
+            data.addDataSet(set);
+        }
+
+        data.addEntry(new Entry(set.getEntryCount(), (float) resistanceValue), 0);
+        data.notifyDataChanged();
+
+        // let the chart know it's data has changed
+        mChart.notifyDataSetChanged();
+
+        // limit the number of visible entries
+        mChart.setVisibleXRangeMaximum(10);
+        // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+        // move to the latest entry
+        mChart.moveViewToX(data.getEntryCount());
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Resistance data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(2f);
+        set.setColor(Color.MAGENTA);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(true);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
+    private void feedMultiple() {
+
+        if (thread != null){
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true){
+                    plotData = true;
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
 
     /*
     A BroadcastReceiver handling various events fired by the Service, see GattActions.Event.
