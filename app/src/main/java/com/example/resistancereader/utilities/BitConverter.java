e321af50da3be9305317ad35a3ba184ea73166f5
Emil Ekelund <emil.ekelund@outlook.com>
2020-06-02 09:45:08 +0200

    update method

--- app/src/main/java/com/example/resistancereader/utilities/BitConverter.java
+++ app/src/main/java/com/example/resistancereader/utilities/BitConverter.java
@@ -2,10 +2,7 @@ package com.example.resistancereader.utilities;
 
 public class BitConverter {
 
-
-    public int[] bytesToResistance(byte[] rawData) {
-
-
-        return null;
+    public double bytesToResistance(byte[] bytes) {
+        return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
     }
 }
