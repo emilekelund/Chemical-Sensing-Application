fe1edfb3648782fe5a29275983047f992315e11d
Emil Ekelund <emil.ekelund@outlook.com>
2020-07-23 15:49:43 +0200

    Add method to convert an array of bytes that we will get from the multichannel board

--- app/src/main/java/com/example/chemicalsensingapplication/utilities/BitConverter.java
+++ app/src/main/java/com/example/chemicalsensingapplication/utilities/BitConverter.java
@@ -5,4 +5,14 @@ public class BitConverter {
     public static double bytesToDouble(byte[] bytes) {
         return bytes[3] << 24 | (bytes[2] & 0xFF) << 16 | (bytes[1] & 0xFF) << 8 | (bytes[0] & 0xFF);
     }
+
+    public static double[] bytesToDoubleArr(byte[] bytes) {
+        double[] multiChannelValues = new double[8];
+
+        for (int i = bytes.length - 1; i > 1; i -= 2) {
+            multiChannelValues[i] = (bytes[i] & 0xFF)  << 8 | (bytes[i-1] & 0xFF);
+        }
+
+        return multiChannelValues;
+    }
 }
