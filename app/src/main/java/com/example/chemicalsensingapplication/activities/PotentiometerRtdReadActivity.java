c6c157c7acc1b4ec3260d1fba780d7d120b488ac
Emil Ekelund <emil.ekelund@outlook.com>
2021-03-19 16:58:18 +0100

    Activity for potentiometer and RTD readout created

---
+++ app/src/main/java/com/example/chemicalsensingapplication/activities/PotentiometerRtdReadActivity.java
@@ -0,0 +1,473 @@
+package com.example.chemicalsensingapplication.activities;
+
+import android.Manifest;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+
+import com.example.chemicalsensingapplication.R;
+import com.example.chemicalsensingapplication.services.BleService;
+import com.example.chemicalsensingapplication.services.GattActions;
+import com.example.chemicalsensingapplication.utilities.MsgUtils;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Objects;
+
+import static com.example.chemicalsensingapplication.services.GattActions.ACTION_GATT_CHEMICAL_SENSING_EVENTS;
+import static com.example.chemicalsensingapplication.services.GattActions.EVENT;
+import static com.example.chemicalsensingapplication.services.GattActions.POTENTIOMETER_RTD_DATA;
+import static com.example.chemicalsensingapplication.services.GattActions.POTENTIOMETRIC_DATA;
+
+public class PotentiometerRtdReadActivity extends AppCompatActivity {
+    private static final String TAG = PotentiometricReadActivity.class.getSimpleName();
+    public static String SELECTED_DEVICE = "Selected device";
+
+    private BluetoothDevice mSelectedDevice = null;
+
+    private TextView mPotentialView;
+    private TextView mTemperatureView;
+    private TextView mStatusView;
+    private TextView mDeviceView;
+    private String mDeviceAddress;
+    private BleService mBluetoothLeService;
+    private ToggleButton mSaveDataButton;
+    private ToggleButton mPauseDataButton;
+
+    private static final DateFormat df = new SimpleDateFormat("yyMMdd_HH:mm"); // Custom date format for file saving
+    private FileOutputStream dataSample = null;
+
+    private ILineDataSet set = null;
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = true;
+
+    private long timeSinceSamplingStart = 0;
+
+    private static final float MULTIPLIER = 0.03125F; // 0.03125 mV per bit
+
+    private final CountDownTimer mCountDownTimer = new
+            CountDownTimer(86400000, 50) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timeSinceSamplingStart = 86400000 - millisUntilFinished;
+                }
+
+                @Override
+                public void onFinish() {
+
+                }
+            };
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_potentiometer_rtd);
+        mPotentialView = findViewById(R.id.potential_viewer);
+        mTemperatureView = findViewById(R.id.temperature_viewer);
+        mStatusView = findViewById(R.id.status_view);
+        mDeviceView = findViewById(R.id.device_view);
+        mSaveDataButton = findViewById(R.id.data_save_toggle);
+        mPauseDataButton = findViewById(R.id.pause_save_toggle);
+
+        // SETTING UP THE TOOLBAR
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Potentiometer/RTD");
+        // TOOLBAR: BACK ARROW
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        // On-click listener for the toggle button used to sample data
+        mSaveDataButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Button is checked, create a new file and start the timer
+                    dataSample = createFiles();
+                    mCountDownTimer.start();
+                    MsgUtils.showToast("Data saving started", getApplicationContext());
+                } else {
+                    try {
+                        // Button is unchecked, close the file
+                        closeFiles(dataSample);
+                        MsgUtils.showToast("Data is now stored on your phone.", getApplicationContext());
+                        mCountDownTimer.cancel();
+                        timeSinceSamplingStart = 0;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        final Intent intent = getIntent();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mSelectedDevice = intent.getParcelableExtra(ScanActivity.SELECTED_DEVICE);
+        }
+
+        if (mSelectedDevice == null) {
+            MsgUtils.createDialog("Error", "No device found", this).show();
+            mDeviceView.setText(R.string.no_potentiometric_board);
+        } else {
+            mDeviceView.setText(mSelectedDevice.getName());
+            mDeviceAddress = mSelectedDevice.getAddress();
+        }
+
+        // Setup UI reference to the chart
+        mChart = findViewById(R.id.potentiometer_chart);
+
+        // enable description text
+        mChart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        // set an alternative background color
+        mChart.setBackgroundColor(Color.WHITE);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.BLACK);
+
+        // add empty data
+        mChart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.BLACK);
+
+        // X-axis setup
+        XAxis bottomAxis = mChart.getXAxis();
+        bottomAxis.setTextColor(Color.BLACK);
+        bottomAxis.setDrawGridLines(true);
+        bottomAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
+        bottomAxis.setAvoidFirstLastClipping(true);
+        bottomAxis.setEnabled(true);
+
+        // Y-axis setup
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.BLACK);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setAxisMaximum(1000f);
+        leftAxis.setAxisMinimum(-1000f);
+        leftAxis.setDrawGridLines(true);
+        // Disable right Y-axis
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        mChart.getAxisLeft().setDrawGridLines(true);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
+
+        feedMultiple();
+
+
+        // Bind to BleImuService
+        // We use onResume or onStart to register a broadcastReceiver
+        Intent gattServiceIntent = new Intent(this, BleService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+
+        isStoragePermissionGranted(); // The user needs to approve the file storing
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+        if (thread != null) {
+            thread.interrupt();
+        }
+        // When the activity is paused, toggle the button so that the files are closed
+        if (mSaveDataButton.isChecked()) {
+            mSaveDataButton.toggle();
+        }
+    }
+
+    /*
+    * NB! Unbind from service when this activity is destroyed (the service itself
+    * might then stop).
+    */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+        thread.interrupt();
+    }
+
+    /*
+    * Callback methods to manage the (BleImu)Service lifecycle.
+    */
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BleService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.i(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+            Log.i(TAG, "onServiceConnected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+            Log.i(TAG, "onServiceDisconnected");
+        }
+    };
+
+    /*
+    A method to add our pH entries to the chart
+     */
+    private void addEntry(double potential) {
+        LineData data = mChart.getData();
+
+
+        if (data != null) {
+            set = data.getDataSetByIndex(0);
+            // set.addEntry(.....); Can be called as well
+        }
+
+        if (set == null) {
+            set = createSet();
+            assert data != null;
+            data.addDataSet(set);
+        }
+
+        assert data != null;
+        data.addEntry(new Entry(set.getEntryCount(), (float) potential), 0);
+        data.notifyDataChanged();
+
+        // let the chart know it's data has changed
+        mChart.notifyDataSetChanged();
+
+        // limit the number of visible entries
+        mChart.setVisibleXRangeMaximum(1000);
+        //mChart.setVisibleYRange(0,30, YAxis.AxisDependency.LEFT);
+
+        // move to the latest entry
+        //mChart.moveViewToX(data.getEntryCount());
+        mChart.moveViewTo(data.getEntryCount(), (float) potential, YAxis.AxisDependency.LEFT);
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Potential Value");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3f);
+        set.setColor(Color.RED);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setCircleRadius(2f);
+        return set;
+    }
+
+    private void feedMultiple() {
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    plotData = true;
+                    try {
+                        Thread.sleep(900);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    /*
+    A BroadcastReceiver handling various events fired by the Service, see GattActions.Event.
+    */
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (ACTION_GATT_CHEMICAL_SENSING_EVENTS.equals(action)) {
+                GattActions.Event event = (GattActions.Event) intent.getSerializableExtra(EVENT);
+                if (event != null) {
+                    switch (event) {
+                        case GATT_CONNECTED:
+                        case GATT_DISCONNECTED:
+                            mPotentialView.setText(R.string.board_disconnected);
+                            mTemperatureView.setText(R.string.board_disconnected);
+                        case GATT_SERVICES_DISCOVERED:
+                        case POTENTIOMETER_RTD_SERVICE_DISCOVERED:
+                            mStatusView.setText(event.toString());
+                            break;
+                        case DATA_AVAILABLE:
+                            final int[] rawValues = intent.getIntArrayExtra(POTENTIOMETER_RTD_DATA);
+                            double potential = rawValues[0] * MULTIPLIER;
+                            double resistance = (rawValues[1] * 6650.0) / Math.pow(2,16) * 2;
+                            double temperature = (resistance - 1000) / (1000 * 0.00385);
+                            mPotentialView.setText(String.format("%.1f mV", potential));
+                            mTemperatureView.setText(String.format("%.1f\u00B0C", temperature));
+
+                            if (plotData) {
+                                addEntry(potential);
+                                plotData = false;
+                            }
+
+                            if (mSaveDataButton.isChecked() && !mPauseDataButton.isChecked()) {
+                                try {
+                                    dataSample.write(((float)timeSinceSamplingStart / 1000f + ",").getBytes());
+                                    dataSample.write((potential + ",").getBytes());
+                                    dataSample.write((resistance + "\n").getBytes());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            break;
+                        case POTENTIOMETER_RTD_SERVICE_UNAVAILABLE:
+                            mStatusView.setText(event.toString());
+                            break;
+                        default:
+                            mStatusView.setText(R.string.device_unreachable);
+                    }
+                }
+            }
+        }
+    };
+
+    // Intent filter for broadcast updates from BleService
+    private IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_GATT_CHEMICAL_SENSING_EVENTS);
+        return intentFilter;
+    }
+
+    // Method to sample data used by the ToggleButton
+    private FileOutputStream createFiles() {
+        // Get the external storage location
+        String root = Environment.getExternalStorageDirectory().toString();
+        // Create a new directory
+        File myDir = new File(root, "/Chemical_sensing_data");
+        if (!myDir.exists()) {
+            myDir.mkdirs();
+        }
+
+        String potentiometric = "potential_temperature_measurement_" + df.format(Calendar.getInstance().getTime()) + ".csv";
+
+        File potentiometricFile = new File(myDir, potentiometric);
+
+        try {
+            potentiometricFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            return new FileOutputStream(potentiometricFile, true);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    // Helper method to close the files.
+    private static void closeFiles(FileOutputStream fo) throws IOException {
+        fo.flush();
+        fo.close();
+    }
+
+    // Method to check if the user has granted access to store data on external memory
+    public boolean isStoragePermissionGranted() {
+        String TAG = "Storage Permission";
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (this.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "Permission is granted");
+                return true;
+            } else {
+                Log.i(TAG, "Permission is revoked");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.i(TAG,"Permission is granted");
+            return true;
+        }
+    }
+}
